dbf762f42b20d790a453f3bf119ba32a835b3152
baran28 <baran28@ca.com>
2018-04-06 09:20:35 -0700

    fixed typo, added more detail to index.jsp

--- src/main/java/com/servlet/BasicServlet.java
+++ src/main/java/com/servlet/BasicServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class BasicServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.getWriter().println("Hello Ana (you are a boss)!!");
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello ANA (you are a boss)!!</h2>
+<h2>Hello ANA (you are a boss) - index.jsp!!</h2>
 </body>
 </html>
